31076536e3bea09cad3a10148f5e62ecf71c9c97
Leo <89185836+djm07073@users.noreply.github.com>
2024-11-10 03:09:16 +0900

    feat: release action (#15)
    * wip
    
    * wip
    
    * fix: build script
    
    * wip
    
    * fix: make dir
    
    * chore: make all actions able
    
    * feat: window
    
    * fix: github action linux and macos
    
    * fix: github action linux and macos
    
    This reverts commit 18c6bc70870590e94547e70bcdb45816e2163004.
    
    * fix: github action linux and macos
    
    This reverts commit ced32599e4dc797afb188b50bd83f4be2ac4d310.
    
    * fix script
    
    * fix: script making directory
    
    * fix: docker image
    
    * fix: make script
    
    * fix: docker image script
    
    * fix: build script
    
    * fix:add docker image llvm ar
    
    * fix: make script
    
    * fix: delete unused dependecies
    
    * feat: github build actions
    
    * fix: github actions

---
+++ builders/guest/build_gnu_aarch64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+mkdir -p artifacts
+export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+
+# No stripping implemented (see https://github.com/CosmWasm/wasmvm/issues/222#issuecomment-2260007943).
+
+echo "Starting aarch64-unknown-linux-gnu build"
+export CC=clang
+export CXX=clang++
+export qemu_aarch64="qemu-aarch64 -L /usr/aarch64-linux-gnu"
+export CC_aarch64_unknown_linux_gnu=clang
+export AR_aarch64_unknown_linux_gnu=llvm-ar
+export CFLAGS_aarch64_unknown_linux_gnu="--sysroot=/usr/aarch64-linux-gnu"
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
+export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER="$qemu_aarch64"
+(cd librevm && cargo build --release --target aarch64-unknown-linux-gnu)
+cp "./target/aarch64-unknown-linux-gnu/release/librevmapi.so" artifacts/librevmapi.aarch64.so
---
+++ builders/guest/build_macos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+# create artifacts directory
+mkdir -p artifacts
+
+export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+
+# ref: https://wapl.es/rust/2019/02/17/rust-cross-compile-linux-to-macos.html
+export PATH="/opt/osxcross/target/bin:$PATH"
+export LIBZ_SYS_STATIC=1
+
+
+echo "Starting aarch64-apple-darwin build"
+export CC=aarch64-apple-darwin20.4-clang
+export CXX=aarch64-apple-darwin20.4-clang++
+
+cargo build --release --target aarch64-apple-darwin
+
+echo "Starting x86_64-apple-darwin build"
+export CC=o64-clang
+export CXX=o64-clang++
+
+cargo build --release --target x86_64-apple-darwin
+
+# Create a universal library with both archs
+lipo -output artifacts/librevmapi.dylib -create \
+  "./target/x86_64-apple-darwin/release/deps/librevmapi.dylib" \
+  "./target/aarch64-apple-darwin/release/deps/librevmapi.dylib"

470d608d6e98f1b284992a8e75749382ec0b8b8e
Ji Hwan <angeljihwan@gmail.com>
2024-11-18 17:28:17 +0900

    feat: separate aot go test

--- lib_test.go
+++ lib_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	revm "github.com/rethmint/revm-api"
 	fibca "github.com/rethmint/revm-api/contracts/Fibonacci"
-	// testca "github.com/rethmint/revm-api/contracts/Test"
+	testca "github.com/rethmint/revm-api/contracts/Test"
 	testutils "github.com/rethmint/revm-api/testutils"
 	types "github.com/rethmint/revm-api/types/go"
 	"github.com/stretchr/testify/require"
@@ -43,82 +43,84 @@ func setupTest(t *testing.T, aot bool) (revm.VM, *testutils.MockKVStore, common.
 	return vm, kvStore, caller
 }
 
-// func Test_e2e(t *testing.T) {
-// 	vm, kvStore, caller := setupTest(t, false)
-//
-// 	// Deploy Test Contract
-// 	txData, err := hexutil.Decode(testca.TestBin)
-// 	require.NoError(t, err)
-// 	createTx := testutils.MockTx(caller, common.Address{}, txData, 0)
-// 	block := testutils.MockBlock(1)
-// 	res, err := vm.ExecuteTx(kvStore, block.ToSerialized(), createTx.ToSerialized())
-// 	require.NoError(t, err)
-// 	result, err := res.ProcessExecutionResult()
-// 	require.NoError(t, err)
-// 	createRes, ok := result.(types.Success)
-// 	require.True(t, ok)
-// 	deployedAddr := createRes.Output.DeployedAddress
-//
-// 	abi, err := testca.TestMetaData.GetAbi()
-// 	require.NoError(t, err)
-// 	increaseInput, err := abi.Pack("increase")
-// 	require.NoError(t, err)
-//
-// 	increaseTx := testutils.MockTx(caller, deployedAddr, increaseInput, 1)
-// 	block = testutils.MockBlock(2)
-// 	res, err = vm.ExecuteTx(kvStore, block.ToSerialized(), increaseTx.ToSerialized())
-// 	require.NoError(t, err)
-//
-// 	increaseRes, ok := result.(types.Success)
-// 	require.Equal(t, types.Success{
-// 		Reason:      "Stop",
-// 		GasUsed:     25516,
-// 		GasRefunded: 0,
-// 		Logs: []types.Log{
-// 			{
-// 				Address: deployedAddr,
-// 				Data: types.LogData{
-// 					Topics: []common.Hash{ // keccack256(increased(uint256,uint256))
-// 						{0x61, 0x99, 0x6f, 0xe1, 0x96, 0xf7, 0x2c, 0xb5, 0x98, 0xc4, 0x83, 0xe8, 0x96, 0xa1, 0x22, 0x12, 0x63, 0xa2, 0x8b, 0xb6, 0x30, 0x48, 0x0a, 0xa8, 0x94, 0x95, 0xf7, 0x37, 0xd4, 0xa8, 0xe3, 0xdf},
-// 					},
-// 					Data: []byte{
-// 						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-// 					},
-// 				},
-// 			},
-// 		},
-// 		Output: types.Output{
-// 			DeployedAddress: [20]byte{},
-// 			Output:          []byte{},
-// 		},
-// 	}, increaseRes)
-//
-// 	// Query
-// 	countQuery, err := abi.Pack("count")
-// 	require.NoError(t, err)
-// 	query := testutils.MockTx(caller, deployedAddr, countQuery, 2)
-// 	block = testutils.MockBlock(2)
-// 	res, err = vm.QueryTx(kvStore, block.ToSerialized(), query.ToSerialized())
-// 	require.NoError(t, err)
-//
-// 	result, err = res.ProcessExecutionResult()
-// 	require.NoError(t, err)
-//
-// 	queryRes, ok := result.(types.Success)
-// 	require.True(t, ok)
-// 	require.Equal(t, types.Success{
-// 		Reason:      "Return",
-// 		GasUsed:     27766,
-// 		GasRefunded: 0,
-// 		Logs:        []types.Log{},
-// 		Output: types.Output{
-// 			DeployedAddress: common.Address{},
-// 			Output: []byte{
-// 				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-// 			},
-// 		},
-// 	}, queryRes)
-// }
+func Test_e2e_non_aot(t *testing.T) {
+	vm := revm.NewVM(CANCUN)
+	kvStore := testutils.NewMockKVStore()
+	caller := common.HexToAddress("0x10")
+
+	// Deploy Test Contract
+	txData, err := hexutil.Decode(testca.TestBin)
+	require.NoError(t, err)
+	createTx := testutils.MockTx(caller, common.Address{}, txData, 0)
+	block := testutils.MockBlock(1)
+	res, err := vm.ExecuteTx(kvStore, block.ToSerialized(), createTx.ToSerialized())
+	require.NoError(t, err)
+	result, err := res.ProcessExecutionResult()
+	require.NoError(t, err)
+	createRes, ok := result.(types.Success)
+	require.True(t, ok)
+	deployedAddr := createRes.Output.DeployedAddress
+
+	abi, err := testca.TestMetaData.GetAbi()
+	require.NoError(t, err)
+	increaseInput, err := abi.Pack("increase")
+	require.NoError(t, err)
+
+	increaseTx := testutils.MockTx(caller, deployedAddr, increaseInput, 1)
+	block = testutils.MockBlock(2)
+	res, err = vm.ExecuteTx(kvStore, block.ToSerialized(), increaseTx.ToSerialized())
+	require.NoError(t, err)
+
+	increaseRes, ok := result.(types.Success)
+	require.Equal(t, types.Success{
+		Reason:      "Stop",
+		GasUsed:     25516,
+		GasRefunded: 0,
+		Logs: []types.Log{
+			{
+				Address: deployedAddr,
+				Data: types.LogData{
+					Topics: []common.Hash{ // keccack256(increased(uint256,uint256))
+						{0x61, 0x99, 0x6f, 0xe1, 0x96, 0xf7, 0x2c, 0xb5, 0x98, 0xc4, 0x83, 0xe8, 0x96, 0xa1, 0x22, 0x12, 0x63, 0xa2, 0x8b, 0xb6, 0x30, 0x48, 0x0a, 0xa8, 0x94, 0x95, 0xf7, 0x37, 0xd4, 0xa8, 0xe3, 0xdf},
+					},
+					Data: []byte{
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+					},
+				},
+			},
+		},
+		Output: types.Output{
+			DeployedAddress: [20]byte{},
+			Output:          []byte{},
+		},
+	}, increaseRes)
+
+	// Query
+	countQuery, err := abi.Pack("count")
+	require.NoError(t, err)
+	query := testutils.MockTx(caller, deployedAddr, countQuery, 2)
+	block = testutils.MockBlock(2)
+	res, err = vm.QueryTx(kvStore, block.ToSerialized(), query.ToSerialized())
+	require.NoError(t, err)
+
+	result, err = res.ProcessExecutionResult()
+	require.NoError(t, err)
+
+	queryRes, ok := result.(types.Success)
+	require.True(t, ok)
+	require.Equal(t, types.Success{
+		Reason:      "Return",
+		GasUsed:     27766,
+		GasRefunded: 0,
+		Logs:        []types.Log{},
+		Output: types.Output{
+			DeployedAddress: common.Address{},
+			Output: []byte{
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+			},
+		},
+	}, queryRes)
+}
 
 func Test_e2e_aot(t *testing.T) {
 	vm, kvStore, caller := setupTest(t, true)
